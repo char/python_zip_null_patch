8b2284334edc64c8ba43fd387d943b61ffb57239
half cambodian hacker man <half-kh-hacker@hackery.site>
2018-08-09 23:25:19 +0100

    Small clean-up of code

--- zip_null_patch.py
+++ zip_null_patch.py
@@ -4,23 +4,19 @@ from types import CodeType
 
 def patch_zipfile():
     code = zipfile.ZipInfo.__init__.__code__
-    payload = code.co_code
+    patched_code = code.co_code
 
-    payload = payload[:6] + bytes([opcode.opmap["NOP"]]) * 34 + payload[40:]
-    new_code = CodeType(code.co_argcount,
-                        code.co_kwonlyargcount,
-                        code.co_nlocals,
-                        code.co_stacksize,
+    patched_code = patched_code[:6] + bytes([opcode.opmap["NOP"]]) * (40 - 6) + patched_code[40:]
+
+    # Since CodeType objects are immutable, we have to create a new one.
+    new_code = CodeType(code.co_argcount, code.co_kwonlyargcount,
+                        code.co_nlocals, code.co_stacksize,
                         code.co_flags,
-                        payload,
+                        patched_code,
                         code.co_consts,
-                        code.co_names,
-                        code.co_varnames,
-                        code.co_filename,
-                        code.co_name,
-                        code.co_firstlineno,
-                        code.co_lnotab,
-                        code.co_freevars,
-                        code.co_cellvars)
+                        code.co_names, code.co_varnames,
+                        code.co_filename, code.co_name,
+                        code.co_firstlineno, code.co_lnotab,
+                        code.co_freevars, code.co_cellvars)
 
     zipfile.ZipInfo.__init__.__code__ = new_code
