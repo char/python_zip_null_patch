843c4f604d933bc96e20b6dc2eb961166f022a8b
half cambodian hacker man <half-kh-hacker@hackery.site>
2018-08-13 17:52:15 +0100

    Clean up patching code

--- zip_null_patch.py
+++ zip_null_patch.py
@@ -2,21 +2,20 @@ import zipfile, opcode
 from types import CodeType
 
 
+def _patch_code_obj(code, custom_co_code):
+    return CodeType(
+        code.co_argcount, code.co_kwonlyargcount, code.co_nlocals,
+        code.co_stacksize, code.co_flags, custom_co_code,
+        code.co_consts, code.co_names, code.co_varnames,
+        code.co_filename, code.co_name, code.co_firstlineno,
+        code.co_lnotab, code.co_freevars, code.co_cellvars
+    )
+
+
 def patch_zipfile():
     code = zipfile.ZipInfo.__init__.__code__
-    patched_code = code.co_code
+    patched_co_code = code.co_code
 
-    patched_code = patched_code[:6] + bytes([opcode.opmap["NOP"]]) * (40 - 6) + patched_code[40:]
-
-    # Since CodeType objects are immutable, we have to create a new one.
-    new_code = CodeType(code.co_argcount, code.co_kwonlyargcount,
-                        code.co_nlocals, code.co_stacksize,
-                        code.co_flags,
-                        patched_code,
-                        code.co_consts,
-                        code.co_names, code.co_varnames,
-                        code.co_filename, code.co_name,
-                        code.co_firstlineno, code.co_lnotab,
-                        code.co_freevars, code.co_cellvars)
-
-    zipfile.ZipInfo.__init__.__code__ = new_code
+    nop_padding = bytes([opcode.opmap["NOP"] for _ in range(34)])
+    patched_co_code = patched_co_code[:6] + nop_padding + patched_co_code[40:]
+    zipfile.ZipInfo.__init__.__code__ = _patch_code_obj(code, patched_co_code)
